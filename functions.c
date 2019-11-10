db2e78cf8b7605258f5bdb679653c088dbb9509d
Nikit910 <57111858+Nikit910@users.noreply.github.com>
2019-11-10 16:07:18 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,27 +1,40 @@
-case'+':
-      printf("%lg + %lg = %lg\n", a, b, a + b);
-  break;
-  case'-';
-      printf("%lg - %lg = %lg\n", a, b, a - b);
-      break;
-  case'*':
-      printf("%lg * %lg = %lg\n", a, b, a * b);
-  break;
-  case'/':
-      result = division( a, b,  &errorCode);
-      if(ErrorCode == 0)
-         printf("%lg\n", result);
-      if(ErrorCode != 0)
-         printf("ой\n");
-         errorCode = 0;
-   break;
-   case 'D':
-      resInt = div((int) a, (int) b, &errorCode);
-      if(0 == errorCode)
-          printf("%lg D %lg = %d\n", a, b, resInt);
-   break;
-   case '%':
-      resInt = mod((int) a, (int) b, &errorCode);
-      if(0 == errorCode)
-          printf("%lg %% %lg = %d\n", a, b, resInt);
-   break;
+#include <math.h>
+/*фу-ия деления берет double a, b, выводит double, ошика при b == 0, тогда меняем и выводим код ошибки*/
+double division(double a, double b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    else
+        *ErrorCode = -1;
+    return 0;
+}
+/* это фу-ия модуля, берет double, a, затем выводит double */
+double module(double a) { 
+    if(a >= 0)
+    return a;
+    else
+    return -a;
+}
+/*фу-ия деления со взятием частного, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
+int div(int a, int b, int* ErrorCode) {
+    if(b != 0)
+        return a / b;
+    if(b == 0)
+        *ErrorCode = -1;
+    return 0;
+}
+/*фу-ия деления со взятием остатка, берет int a, b, выводит int, ошика при b == 0, тогда меняем и выводим код ошибки*/
+int mod(int a, int b, int* ErrorCode){
+    if(b != 0)
+        return a % b;
+    else
+        *ErrorCode = -1;
+    return 0;
+}
+/*функция квадратного корня, берет double a, указатель на errorcode, выводит double, ошибка при a < 0, то указатель на ошипку*/
+double computesqrt(double a, int* ErrorCode) {
+    if(a >= 0)
+        return sqrt(a);
+    else
+        *ErrorCode = -1;
+    return 0;
+}
