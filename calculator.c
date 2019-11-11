e5945984a7d096f283e5acff38603a61577af30f
Nikit910 <57111858+Nikit910@users.noreply.github.com>
2019-11-11 14:15:57 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -11,33 +11,33 @@ while(1){
   scanf("%lg%c%lg", &a, &cmd, &b);
   
   switch(cnd){
-  case'+':
+    case'+':
       printf("%lg + %lg = %lg\n", a, b, a + b);
-  break;
-  case'-';
+      break;
+    case'-':
       printf("%lg - %lg = %lg\n", a, b, a - b);
       break;
-  case'*':
+    case'*':
       printf("%lg * %lg = %lg\n", a, b, a * b);
-  break;
-  case'/':
+      break;
+    case'/':
       result = division( a, b,  &errorCode);
       if(ErrorCode == 0)
-         printf("%lg\n", result);
+        printf("%lg\n", result);
       if(ErrorCode != 0)
-         printf("ой\n");
-         errorCode = 0;
-   break;
-   case 'D':
+        printf("ой\n");
+      errorCode = 0;
+      break;
+    case 'D':
       resInt = div((int) a, (int) b, &errorCode);
       if(0 == errorCode)
-          printf("%lg D %lg = %d\n", a, b, resInt);
-   break;
-   case '%':
+        printf("%lg D %lg = %d\n", a, b, resInt);
+      break;
+    case '%':
       resInt = mod((int) a, (int) b, &errorCode);
       if(0 == errorCode)
-          printf("%lg %% %lg = %d\n", a, b, resInt);
-   break;
- return 0;
+        printf("%lg %% %lg = %d\n", a, b, resInt);
+      break;
+      return 0;
  }
 }
